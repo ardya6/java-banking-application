6fa5b85935e96bfee00598cd873df0d3cbe2694d
ardya6 <103302910+ardya6@users.noreply.github.com>
2024-03-21 16:10:52 +0700

    Add files via upload

---
+++ AtmGUI/src/bank/BANK.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author user
+ */
+public class BANK {
+    private double hasil;
+    private String jenis;
+    private String answer;
+    private double saldo = 5000000;
+    private int masukan;
+    private String nama3;
+    private String norek;
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+   
+
+
+    public double getHasil() {
+        return hasil;
+    }
+
+    public void setHasil(double hasil) {
+        this.hasil = hasil;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getMasukan() {
+        return masukan;
+    }
+
+    public void setMasukan(int masukan) {
+        this.masukan = masukan;
+    }
+
+    public String getNama3() {
+        return nama3;
+    }
+
+    public void setNama3(String nama3) {
+        this.nama3 = nama3;
+    }
+
+    public String getNorek() {
+        return norek;
+    }
+
+    public void setNorek(String norek) {
+        this.norek = norek;
+    }
+    
+}
